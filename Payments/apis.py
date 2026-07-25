f9376d15fc37ea727f2e08d0485f8652d744aff2
Brian JD <brian75jd@gmail.com>
2026-07-25 16:12:28 +0800

    seperated the apis from template rendering views

---
+++ Payments/apis.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.views import APIView
+import logging
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiExample
+)
+from Payments.serializers import (
+    InitiatePaymentResponseSerializer,
+    View404ResponseSerializer,
+    PaymentUserCredentials
+)
+
+m Payments.services.Payments import PaychanguInitiatePayment
+logger = logging.getLogger(__name__)
+
+
+@extend_schema(
+    request=PaymentUserCredentials,
+    responses={
+        200: InitiatePaymentResponseSerializer,
+        400: View404ResponseSerializer
+    }
+)
+class InitiatePayment(APIView):
+    authentication_classes = []
+
+    @staticmethod
+    def post(request, *args, **kwargs):
+        try:
+            serializer = PaymentUserCredentials(data=request.data)
+
+            serializer.is_valid(raise_exception=True)
+            checkout = PaychanguInitiatePayment.initiate_payment(
+                validated_data=serializer.validated_data
+            )
+
+            if checkout.get('success'):
+                return Response({
+                    'success':True,
+                    'checkout_url': checkout.get('checkout_url')
+                })
+
+        except Exception:
+            logger.exception('ERROR: ')
+            raise
--- Payments/urls.py
+++ Payments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from Payments import views
-
+from Payments import apis
 
 urlpatterns = [
-    path('v1/initiate_payment/',views.InitiatePayment.as_view(),name='initate_payment')
+    path('v1/initiate_payment/',apis.InitiatePayment.as_view(),name='initate_payment')
 ]
 
 
