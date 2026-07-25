851604f268d484b6ff64ec56ddfc6c4e6d911738
Jogtrot34 <josephwella35@gmail.com>
2026-07-25 20:34:16 +0200

    Added schools urls

--- edupay/urls.py
+++ edupay/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('school/change-password/', change_password_view, name='change_password'),
     path('admin/', admin.site.urls),
     path('api/payment/', include('Payments.urls')),
+    path('api/', include('schools.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
